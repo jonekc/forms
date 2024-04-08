f9ce348df5ea6d52f5c67ca13c9df24c98a17d8a
jonekc <czapla.jonatan@gmail.com>
2024-04-08 22:20:58 +0200

    add ability to edit a post, delete

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,23 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactMarkdown from 'react-markdown';
 import { PostWithAuthor } from '../types/post';
+import { fetcher, mutateResponse } from '../utils/client/api';
+import useSWR, { mutate } from 'swr';
+import { User } from '@prisma/client';
 
 export type PostProps = {
   post: PostWithAuthor;
 };
 
 const Post: React.FC<PostProps> = ({ post }) => {
+  const [editing, setEditing] = React.useState(false);
+  const [title, setTitle] = React.useState(post.title);
+  const [authorId, setAuthorId] = React.useState(post.author?.id);
+  const [content, setContent] = React.useState(post.content || '');
+  const [published, setPublished] = React.useState(post.published || false);
+
+  const { data: users } = useSWR<User[]>('/api/users', fetcher);
+
+  const handleSave = async () => {
+    await mutateResponse(`/api/posts/${post.id}`, 'PATCH', {
+      title,
+      authorId: authorId || null,
+      content,
+      published,
+    });
+    await mutate('/api/posts');
+    setEditing(false);
+  };
+
+  const handleDelete = async () => {
+    await mutateResponse(`/api/posts/${post.id}`, 'DELETE');
+    await mutate('/api/posts');
+    setEditing(false);
+  };
+
   return (
     <div>
-      <h2>{post.title}</h2>
-      {post.author && (
+      {editing ? (
         <>
-          <small>By {post.author.name}</small>
-          <br />
+          <input value={title} onChange={(e) => setTitle(e.target.value)} />
+          <select
+            value={authorId}
+            onChange={(e) => setAuthorId(e.target.value)}
+          >
+            <option value="">No author</option>
+            {users?.map((user) => <option value={user.id}>{user.name}</option>)}
+          </select>
+          <textarea
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
+          <label>
+            <input
+              type="checkbox"
+              checked={published}
+              onChange={(e) => setPublished(e.target.checked)}
+            />
+            Published
+          </label>
+          <button onClick={handleSave}>Save</button>
+          <button onClick={handleDelete}>Delete</button>
+        </>
+      ) : (
+        <>
+          <h2>{post.title}</h2>
+          {post.author && (
+            <>
+              <small>By {post.author.name}</small>
+              <br />
+            </>
+          )}
+          {post.published ? null : <small>Draft</small>}
+          <ReactMarkdown children={post.content || ''} />
+          <button
+            onClick={() => {
+              setEditing(true);
+            }}
+          >
+            Edit
+          </button>
         </>
       )}
-      {post.published ? null : <small>Draft</small>}
-      <ReactMarkdown children={post.content || ''} />
       <style jsx>{`
         div {
           color: inherit;
