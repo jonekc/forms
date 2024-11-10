2567292097f4702f88e0454e701d01b19fed3578
Jonatan Czapla <87189829+jonekc@users.noreply.github.com>
2024-11-10 16:00:39 +0100

    Fix tests (#17)
    * save failed test screenshots
    
    * increase the request timeout

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     baseUrl: process.env.CYPRESS_BASE_URL || 'http://localhost:3000',
+    requestTimeout: 10000,
     setupNodeEvents(_on, _config) {
       // implement node event listeners here
     },
